6ac82b5701015712ee8c932b9cb0ee26645c7350
Winston Huang <wh5480@gmail.com>
2014-05-25 02:30:45 -0700

    dummy card/deck classes

---
+++ HSTracker/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSTracker
+{
+    class Card
+    {
+        private string _name;
+        private uint _mana;
+
+        public Card(string name, uint mana)
+        {
+            _name = name;
+            _mana = mana;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public uint Mana
+        {
+            get { return _mana; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0}) {1}", _mana, _name);
+        }
+    }
+}
---
+++ HSTracker/Deck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSTracker
+{
+    class Deck
+    {
+        private List<Card> _cards = new List<Card>();
+
+        public Deck(List<Card> cards)
+        {
+            _cards = cards;
+        }
+
+        public List<Card> Cards
+        {
+            get
+            {
+                return _cards;
+            }
+        }
+
+        public void Play(string name)
+        {
+            _cards.Remove(_cards.FirstOrDefault<Card>(x => x.Name == name));
+        }
+    }
+}
